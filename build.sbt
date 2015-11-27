e287ed9376f4755da718da870e7e2b0d0da6eaef
Luchesar Cekov <luchesar@gmail.com>
2015-11-27 11:55:46 +0000

    Recieving the first tweets

--- build.sbt
+++ build.sbt
@@ -5,9 +5,12 @@ version := "1.0-SNAPSHOT"
 scalaVersion := "2.11.7"
 
 val coreNlpVersion = "3.5.2"
+val sparkVersion = "1.5.2"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core"        % "1.5.2",
+  "org.apache.spark" %% "spark-core"        % sparkVersion,
+  "org.apache.spark" %% "spark-streaming"   % sparkVersion,
+  "org.apache.spark" %% "spark-streaming-twitter"   % sparkVersion,
   "edu.stanford.nlp" %  "stanford-corenlp"  % coreNlpVersion,
   "edu.stanford.nlp" %  "stanford-corenlp"  % coreNlpVersion classifier "models",
   "edu.stanford.nlp" %  "stanford-corenlp"  % coreNlpVersion classifier "models-chinese",
--- src/main/scala/SocialMeter.scala
+++ src/main/scala/SocialMeter.scala
@@ -1,16 +1,30 @@
 /* SimpleApp.scala */
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.twitter.TwitterUtils
 
 object SocialMeter {
   def main(args: Array[String]) {
-    val logFile = "/Users/luchesar.cekov/prj/socialmeter/src/main/scala/SocialMeter.scala" // Should be some file on your system
-    val conf = new SparkConf().setAppName("SocialMeter").setMaster("local[2]")
-    val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+
+    sys.props ++= Map(
+      "twitter4j.oauth.consumerKey" -> "e0AxEdyyY6JIqBvEpI7HLnRIH",
+      "twitter4j.oauth.consumerSecret" -> "PIOaJHEfBctDSQq6vn5iE2ntJFnGP0kKXAJtNP0CfLYwlX15Vt",
+      "twitter4j.oauth.accessToken" -> "2752424182-3O9t4gEYZYC5uD04QEvAN2Y9rg6E1Rz8hFyY0NY",
+      "twitter4j.oauth.accessTokenSecret" -> "6bV28Z7y2hPXm8MX3YbJr6PP2XSIn36TRerAhPC9JSUzG"
+    )
+
+    val conf = new SparkConf().setMaster("local[2]").setAppName("SocialMeter")
+    val ssc = new StreamingContext(conf, Milliseconds(500))
+
+    val tweets = TwitterUtils.createStream(ssc, None, Seq("#bitcoin"))
+    val statuses = tweets.map { tweet =>
+      println("TWEET: " + tweet.getText)
+      tweet.getText
+    }
+
+    statuses.print()
+
+    ssc.start()
+    Thread.sleep(50000000)
   }
 }
